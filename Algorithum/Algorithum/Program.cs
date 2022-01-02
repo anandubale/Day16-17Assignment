e48a17fc224475b51975c7ba8417203261592d3d
anandubale <au26199@gmail.com>
2022-01-02 11:43:58 +0530

    Assignment has algorithum for different Problems

---
+++ Algorithum/Algorithum/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Algorithum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Algorithm Programs");
+            UserInput userInput = new UserInput();
+
+        }
+    }
+}
